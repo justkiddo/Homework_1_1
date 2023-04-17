50dfa377cb17f36f3d692b64bbc2371ca0cbdd00
Ivan K <ki93@yandex.ru>
2023-04-17 22:40:56 +0300

    1.5 bulletPower

--- Assets/Scripts/Canon/PlayerMovement.cs
+++ Assets/Scripts/Canon/PlayerMovement.cs
@@ -11,12 +11,11 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float movementSpeed = 5;
     [Range(5,100)]
     [SerializeField] private float rotateSpeed = 5;
-    [Range(1000f,10000f)]
-    [SerializeField] private float bulletSpeed = 1000f;
     [SerializeField] private Transform shootPoint;
     [SerializeField] private GameObject bullet;
-    [SerializeField] private Slider shootingAngleSlider;
-    
+    [SerializeField] private Slider shootingAngleSlider; 
+    private float startShooting, stoppedShooting;
+    private float bulletSpeed = 1000f;
     private void OnValidate()
     {
         rb = GetComponent<Rigidbody>();
@@ -31,14 +30,26 @@ public class PlayerMovement : MonoBehaviour
     {
         var horizontal = Input.GetAxis("Horizontal");
         var vertical = Input.GetAxis("Vertical");
- 
+        
+        
         rb.angularVelocity = horizontal * rotateSpeed * transform.up;
         rb.velocity = vertical * movementSpeed * -transform.forward;
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            GameObject _bullet = Instantiate(bullet, shootPoint.position, Quaternion.identity);
-            _bullet.GetComponent<Rigidbody>().AddForce(-transform.forward * bulletSpeed, ForceMode.Force);
+             startShooting = Time.time;
+           
         }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            stoppedShooting = Time.time;
+            var bulletPower = bulletSpeed * (stoppedShooting - startShooting);
+            Debug.Log(startShooting + "  " + stoppedShooting);
+            if (!(bulletPower < 10000f)) return;
+            GameObject _bullet = Instantiate(bullet, shootPoint.position, Quaternion.identity);
+            _bullet.GetComponent<Rigidbody>().AddForce(-transform.forward * bulletPower, ForceMode.Force);
+        }
+        
+        
     }
 
     private void onShottingAngleChange(float value)
@@ -46,5 +57,6 @@ public class PlayerMovement : MonoBehaviour
         var scaling = value * 2;
         rb.transform.Rotate(scaling,0,0);
     }
-    
+
+
 }
