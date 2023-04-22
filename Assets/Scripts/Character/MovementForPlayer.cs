95dd18f0f4f678d6ff4559667045acd9d8d2e754
Ivan K <ki93@yandex.ru>
2023-04-22 15:57:41 +0300

    TerraiMovement

--- Assets/Scripts/Character/MovementForPlayer.cs
+++ Assets/Scripts/Character/MovementForPlayer.cs
@@ -1,30 +1,49 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody))]
+
+[RequireComponent(typeof(CharacterController))]
 public class MovementForPlayer : MonoBehaviour
 {
-    [SerializeField, HideInInspector] private Rigidbody _rb;
-    [SerializeField] private Animator _animator;
-    private float movementSpeed = 2f;
-    private float rotateSpeed = 1f;
-    private static readonly int IsWalking = Animator.StringToHash("isWalking");
-
-    // Start is called before the first frame update
-    void Start()
+    [HideInInspector] [SerializeField] private CharacterController _movement;
+    [SerializeField] private float moveSpeed = 10f;
+    private Vector3 _velocity = Vector3.zero;
+    private void OnValidate()
     {
-        
+        _movement = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
         var horizontal = Input.GetAxis("Horizontal");
         var vertical = Input.GetAxis("Vertical");
+        var mouseX = Input.GetAxis("Mouse X");
+        var mouseY = Input.GetAxis("Mouse Y");
+        var jump = Input.GetAxis("Jump");
+        RotateCharacter(mouseX, mouseY);
+        Moving(horizontal, vertical);
+    }
+
+    private void RotateCharacter(float mouseX, float mouseY)
+    {
+        transform.Rotate(Vector3.up, mouseX );
+        transform.Rotate(Vector3.left, mouseY);
+    }
+
+    private void Moving(float horizontal, float vertical)
+    {
+        var dir = new Vector3(horizontal, 0, vertical);
+        var moveVector = transform.TransformDirection(dir) * moveSpeed;
         
-        _rb.angularVelocity = horizontal * rotateSpeed * transform.up;
-        _rb.velocity = vertical * movementSpeed * transform.forward;
+        if (_movement.isGrounded && _velocity.y < 0)
+        {
+            _velocity.y = 0f;
+        }
+
+        _velocity.y += -9.81f * Time.deltaTime;
         
+        _movement.Move((moveVector + _velocity) * Time.deltaTime);
     }
 }
