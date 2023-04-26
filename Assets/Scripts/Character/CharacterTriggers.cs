38bd5cecd4c16e05e433292fe3e93780705b99a8
Ivan K <ki93@yandex.ru>
2023-04-26 21:29:19 +0300

    1.6 DeathCam

--- Assets/Scripts/Character/CharacterTriggers.cs
+++ Assets/Scripts/Character/CharacterTriggers.cs
@@ -7,6 +7,8 @@ public class CharacterTriggers : MonoBehaviour
     [SerializeField] private Animator animator;
     [SerializeField] private MovementForPlayer _movementForPlayer;
     [SerializeField] private GameObject deathPanel;
+    [SerializeField] private GameObject deathCam;
+    [SerializeField] private GameObject mainCam;
     private static readonly int IsDead = Animator.StringToHash("IsDead");
 
     
@@ -16,7 +18,10 @@ public class CharacterTriggers : MonoBehaviour
         animator.SetBool(IsDead, true);
 
             _movementForPlayer.enabled = false;
-            deathPanel.SetActive(true);
+            mainCam.SetActive(false);
+            deathCam.SetActive(true);
+            
+            // deathPanel.SetActive(true);
 
     }
 }
